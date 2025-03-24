37ce21f86a10e8157bbea92a01db265fd022e349
mylenaOliveiras <myholiveira6@gmail.com>
2025-03-24 15:12:35 -0300

    refactor: remove PostgreSQL configuration and replace with MySQL setup

--- prisma/migrations/20250319162851_teste/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "teste" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "teste_pkey" PRIMARY KEY ("id")
-);
--- src/repositories/ITesteRepository.ts
+++
@@ -1,6 +0,0 @@
-import type { ITeste } from "../entities/Teste";
-
-interface ITesteRepository {
-	register(teste: ITeste): Promise<void>;
-}
-export type { ITesteRepository };
