89ef6e533c75eaa42a2b7ae7a840e04c7285b0af
mylenaOliveiras <myholiveira6@gmail.com>
2025-04-15 10:58:17 -0300

    feat: implement vehicle ownership validation and refactor service methods for better error handling

--- src/services/vehicles.service.ts
+++ src/services/vehicles.service.ts
@@ -1,8 +1,19 @@
+import type { veiculos } from "@prisma/client";
 import { VehiclesRepository } from "../repositories/vehicles.repository";
 import type { INewVehicle, IVehicleSimple } from "../schemas/vehicles.schema";
 import { AppError } from "../utils/AppError";
 
 export class VehiclesService {
+	static validateOwnership(vehicle: veiculos | null, userId: number) {
+		if (!vehicle) {
+			throw new AppError("Veículo não encontrado", 404);
+		}
+
+		if (vehicle.USUARIO_ID !== userId) {
+			throw new AppError("Veículo não pertence ao usuário", 403);
+		}
+	}
+
 	static async list(userId: number): Promise<IVehicleSimple[]> {
 		if (!userId || userId === 0) {
 			throw new AppError("Usuário não identificado", 400);
@@ -43,18 +54,12 @@ export class VehiclesService {
 		const vehicleIdParsed = Number.parseInt(vehicleId);
 
 		const vehicle = await VehiclesRepository.findById(vehicleIdParsed);
-		const isUserOwner = vehicle?.USUARIO_ID === userId;
 
 		if (!userId || userId === 0) {
 			throw new AppError("Usuário não identificado", 400);
 		}
 
-		if (!vehicle || !isUserOwner) {
-			throw new AppError(
-				"Veículo não encontrado ou não pertence ao usuário",
-				403,
-			);
-		}
+		VehiclesService.validateOwnership(vehicle, userId);
 
 		await VehiclesRepository.delete(vehicleIdParsed);
 
@@ -69,14 +74,7 @@ export class VehiclesService {
 		const vehicleIdParsed = Number.parseInt(vehicleId);
 
 		const vehicle = await VehiclesRepository.findById(vehicleIdParsed);
-		const isUserOwner = vehicle?.USUARIO_ID === userId;
-
-		if (!vehicle || !isUserOwner) {
-			throw new AppError(
-				"Veículo não encontrado ou não pertence ao usuário",
-				403,
-			);
-		}
+		VehiclesService.validateOwnership(vehicle, userId);
 
 		const registeredVehicle = await VehiclesRepository.findByLicensePlate(
 			vehicleData.placaVeiculo,
@@ -97,27 +95,20 @@ export class VehiclesService {
 	static async findById(userId: number, vehicleId: string) {
 		const vehicleIdParsed = Number.parseInt(vehicleId);
 		const vehicle = await VehiclesRepository.findById(vehicleIdParsed);
-		const isUserOwner = vehicle?.USUARIO_ID === userId;
-
-		if (!vehicle || !isUserOwner) {
-			throw new AppError(
-				"Veículo não encontrado ou não pertence ao usuário",
-				403,
-			);
-		}
+		VehiclesService.validateOwnership(vehicle, userId);
 
 		const formattedVehiclesList = {
-			id: vehicle.ID,
-			marca: vehicle.MARCA_ID,
-			modelo: vehicle.MODELO,
-			cor: vehicle.COR,
-			anoModelo: vehicle.ANO_MODELO,
-			anoFabricacao: vehicle.ANO_FABRICACAO,
-			placaVeiculo: vehicle.PLACA_VEICULO,
-			kmInicial: vehicle.KM_INICIAL,
-			dataCadastro: vehicle.DATA_CADASTRO,
-			tipoVeiculo: vehicle.TIPO_VEICULO,
-			combustivel: vehicle.TIPO_COMBUSTIVEL,
+			id: vehicle?.ID,
+			marca: vehicle?.MARCA_ID,
+			modelo: vehicle?.MODELO,
+			cor: vehicle?.COR,
+			anoModelo: vehicle?.ANO_MODELO,
+			anoFabricacao: vehicle?.ANO_FABRICACAO,
+			placaVeiculo: vehicle?.PLACA_VEICULO,
+			kmInicial: vehicle?.KM_INICIAL,
+			dataCadastro: vehicle?.DATA_CADASTRO,
+			tipoVeiculo: vehicle?.TIPO_VEICULO,
+			combustivel: vehicle?.TIPO_COMBUSTIVEL,
 		};
 
 		return formattedVehiclesList;
